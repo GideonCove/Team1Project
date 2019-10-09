c972bd62debf1b687f10649e49283e1c395b0da7
BHPirkle <sethrhowell@gmail.com>
2019-10-09 13:45:21 -0400

    Added player and camera movement scripts

---
+++ Assets/Scripts/Player/cameraRotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraRotation : MonoBehaviour
+{
+    // Sensitivity for the movement looking horizontally (x-axis) and vertically (y-axis).
+    public float sensitivityHorizontal = 10f;
+    public float sensitivityVertical = 10f;
+
+    // This optional float allows us to disable vertical movement in the case we use it for the player's rotation.
+    public float optional = 1f;
+
+    // The vertical angle at which the player is looking.
+    public float rotationX = 60f;
+
+    // Clamping restrictions.
+    public float minVertical = -45f;
+    public float maxVertical = 45f;
+
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHorizontal, 0);
+        rotationX -= (Input.GetAxis("Mouse Y") * sensitivityVertical) * optional;
+
+        // This clamps the vertical angle.
+        rotationX = Mathf.Clamp(rotationX, minVertical, maxVertical);
+
+        float rotationY = transform.localEulerAngles.y;
+
+        // Create a new vector for the stored rotation values.
+        transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+    }
+}
---
+++ Assets/Scripts/Player/playerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // This script is used to allow the arrow keys to move the player around the game world on the x and z-axes.
+    public Vector3 xVector = new Vector3(1f, 0f, 0f);
+    public Vector3 zVector = new Vector3(0f, 0f, 1f);
+    public float moveSpeed = 7f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Increase the position on the x-axis.
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(-xVector * moveSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(xVector * moveSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-zVector * moveSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(zVector * moveSpeed * Time.deltaTime, Space.Self);
+        }
+    }
+}
