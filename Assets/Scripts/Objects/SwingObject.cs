3724a3d8bd48f1bcc0cc0c32ac1d85f583ab9eb3
Eddyzim1234 <56129050+Eddyzim1234@users.noreply.github.com>
2019-11-14 12:53:04 -0500

    Created a script called SwingObject for the swings in the playground

---
+++ Assets/Scripts/Objects/SwingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingObject : MonoBehaviour
+{
+    public float delta = 1.5f;
+    public float speed = 2.0f;
+    public float direction = 1;
+    private Quaternion startPos;
+    void Start()
+    {
+        startPos = transform.rotation;
+    }
+    void Update()
+    {
+        Quaternion a = startPos;
+        a.z += direction * (delta * Mathf.Sin(Time.time * speed));
+        transform.rotation = a;
+    }
+}
