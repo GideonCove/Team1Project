49297178c75793259d2a07ca1cacef88ca6e0f20
GideonCove <48099028+GideonCove@users.noreply.github.com>
2019-11-26 13:37:22 -0500

    added orbit script

---
+++ Assets/Scripts/Objects/Orbit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Authors: Mary Rasley, Gideon Cove
+public class Orbit : MonoBehaviour
+{
+    public GameObject insideSun;
+    void Start()
+    {
+        insideSun = GameObject.Find("centerSolarSystem");
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        orbitAround();
+        spinOnTheAxis();
+    }
+
+    void orbitAround()
+    {
+        transform.RotateAround(insideSun.transform.position, new Vector3(0, 0, 1f), 45f * Time.deltaTime);
+    }
+
+    void spinOnTheAxis()
+    {
+        transform.RotateAround(transform.position, new Vector3(0, 1, 0), 500f * Time.deltaTime);
+    }
+}
