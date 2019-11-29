bd0029e677d93d5dec7121360dad71f2fc19f5b1
BHPirkle <sethrhowell@gmail.com>
2019-11-29 11:31:35 -0500

    Added more gym enhancement and created placeholder principals enhancement as well as the sound for it

---
+++ Assets/Scripts/Enhancements/BasketballSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * AUTHOR(S): Seth
+ * DATE OF CREATION: 11/29/2019
+ * SCENE(S) WHERE USED: gym
+ * OBJECT(S) WHERE USED: loose_basketball
+ * DESCRIPTION: Checks whether the ball was already collected or not and, if so, deletes it.
+ */
+
+public class BasketballSpawn : MonoBehaviour
+{
+    private static bool gotBall = false;
+
+    private void Awake()
+    {
+        if (gotBall)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            gotBall = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Enhancements/PrincipalScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * AUTHOR(S): Seth
+ * DATE OF CREATION: 11/29/2019
+ * SCENE(S) WHERE USED: principals
+ * OBJECT(S) WHERE USED: scene_trigger
+ * DESCRIPTION: Creates a little scene when entering the principal's office.
+ */
+
+public class PrincipalScene : MonoBehaviour
+{
+    private static bool played = false;
+}
